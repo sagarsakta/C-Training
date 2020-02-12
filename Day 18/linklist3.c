4fdfaa8f0f78d862d725ddedf8d515db2bb7d76f
sagarsakta <sagarsakta98@gmail.com>
2020-02-12 04:43:23 -0800

    committed

---
+++ Day 18/linklist3.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+        struct node{
+                int data;
+                struct node *next;
+        };
+
+        struct node *head=NULL,*temp, *newnode;
+        int choice=1;
+
+        while(choice)
+        {
+         newnode =(struct node *) malloc(sizeof(struct node));
+        printf("enter the data u want");
+        scanf("%d", &newnode->data);
+        newnode->next=0;
+
+
+        if(head==NULL)
+        {
+          head=newnode;
+
+          temp=head;
+        }
+        else
+        {
+                temp->next=newnode;
+                temp=newnode;
+        }
+         printf("do you want to continue?(0,1)");
+        scanf("%d", &choice);
+
+
+        }
+
+	  temp=head;
+
+        while(temp!=0)
+        {
+                printf("%d ",temp->data);
+                temp=temp->next;
+        }
+
+
+	  printf("do you want to delete list at beggining?(0,1)");
+          scanf("%d",&choice);
+
+
+	while(choice)
+	{
+          if(head==NULL)
+		{
+			printf("List is empty");
+		}
+		else
+		{
+			temp=head;
+			head=head->next;
+			
+		}
+		choice=0;
+	}
+	 temp=head;
+
+        while(temp!=0)
+        {
+                printf("%d ",temp->data);
+                temp=temp->next;
+        }
+
+
+
+
+        return 0;
+}                                         
