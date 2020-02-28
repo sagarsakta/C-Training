21d7ca68842c7f7c4fa1454792a8bc1e912db060
sagarsakta <sagarsakta98@gmail.com>
2020-02-28 00:57:12 -0800

    committed

---
+++ Day 29/access1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main(int argc, char *argv[])
+{
+	printf("%d %d\n",getuid(),getgid());
+   if (argc != 2)
+      printf("usage: a.out <pathname>");
+   if (access(argv[1], R_OK) < 0)
+      printf("access error for %s", argv[1]);
+   else
+      printf("read access OK\n");
+   if (open(argv[1], O_RDONLY) < 0)
+      printf("open error for %s", argv[1]);
+   else
+      printf("open for reading OK\n");
+   
+     exit(0);
+}
